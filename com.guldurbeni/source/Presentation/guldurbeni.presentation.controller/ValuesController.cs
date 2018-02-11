825697ef2536379a34eb30248d7cbe4401e20ea4
TK <kupelitufan@gmail.com>
2018-02-11 16:37:12 +0300

    new Controller added

--- com.guldurbeni/source/Presentation/guldurbeni.presentation.controller/ValuesController.cs
+++ com.guldurbeni/source/Presentation/guldurbeni.presentation.controller/ValuesController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
-namespace guldurbeni.presentation.api.Controllers
+namespace guldurbeni.presentation.Controllers
 {
     public class ValuesController : ApiController
     {
